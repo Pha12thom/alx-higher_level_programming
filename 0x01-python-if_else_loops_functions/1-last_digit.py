1f86451851fcae9d7616a023d56344944242632e
pha12thom <geofreymilugo@gmail.com>
2023-08-08 16:43:49 -0700

    if statements with conditions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
+
+# Extract the last digit of the number
 last_digit = abs(number) % 10
+
+# Determine the condition based on the last digit
 if last_digit > 5:
     condition = "and is greater than 5"
 elif last_digit == 0:
     condition = "and is 0"
 else:
-    condition = f"and is less than 6 and not 0" 
-    print(f"Last digit of {number} is {last_digit} {condition}")
+    condition = f"and is less than 6 and not 0"
+    print(f"The string Last digit of, {number} is, {last_digit}, {condition}")
