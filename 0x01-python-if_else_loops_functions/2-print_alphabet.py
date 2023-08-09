e10559ef2949c865d60cfb3a7c77093e6cf5c417
pha12thom <geofreymilugo@gmail.com>
2023-08-09 06:27:49 -0700

    lowercase alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
-for c in range(ord("a"), ord("z") + 1):
-        print(chr(c), end=" ")
+#!/usr/bin/python3
+for c in range(ord("a"), ord("z")):
+    print(chr(c), end=" ")
 print()
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+for c in "abcdefghijklmnopqrstuvwxyz":
+    if c not in "qe":
+        print(c, end=" ")
+print()
