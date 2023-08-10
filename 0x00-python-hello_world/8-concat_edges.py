c7a97b4f3657ee830fa5c7dc15d6407b145acf7e
pha12thom <geofreymilugo@gmail.com>
2023-08-10 06:23:55 -0700

    split and join

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-str = " ".join(str.split()[5:7]+["with"]+["python"])
+str = " ".join(str.split()[5:7]+["with Python"])
 print(str)
