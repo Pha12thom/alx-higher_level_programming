baad5bcc88ce26fecbaea5dd9eb5db7bc4b99edd
pha12thom <geofreymilugo@gmail.com>
2023-08-10 07:02:14 -0700

    compiled printing

--- 0x01-python-if_else_loops_functions/100-write.py
+++ 0x01-python-if_else_loops_functions/100-write.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-sys.exit(1)
+def writting():
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+    sys.exit(1)
+if __name__ == "__main__":
+    writting()
