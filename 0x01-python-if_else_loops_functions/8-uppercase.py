c88eec67b8c42a9a80ab89cd3aa50d9cc44e5433
pha12thom <geofreymilugo@gmail.com>
2023-08-10 03:44:44 -0700

    pushing uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(s):
     formatted_string = ""
     for c in s:
         formatted_string = "{}".format(chr(ord(c) - 32)) if 'a' <= c <= 'z' else c
-        print("{}".format(formatted_string))
+        print("{}".format(formatted_string), end="")
