9869c59d8739b01b9c1eaee9b07f8e82d99fb4b8
pha12thom <geofreymilugo@gmail.com>
2023-08-10 07:31:40 -0700

    hello write

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+def writting():
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+    exit(1)
+if __name__ == "__main__":
+    writting()
