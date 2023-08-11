b408c05c1e52fe71746f2f9ca339b9b6cb449243
pha12thom <geofreymilugo@gmail.com>
2023-08-11 14:20:53 -0700

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    """number of arguments"""
+    num_args = len(sys.argv) - 1
+    """printing what the argument does"""
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument.")
+    else:
+        """printing the number of argument"""
+        print("{} arguments:".format(num_args))
+        for i in range(num_args):
+            """we find the value of argument"""
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))        
