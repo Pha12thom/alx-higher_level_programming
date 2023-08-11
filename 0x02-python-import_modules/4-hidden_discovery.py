23bc0f3537f0e61e6bfa5f6fbe278b576e21e24c
pha12thom <geofreymilugo@gmail.com>
2023-08-11 15:39:03 -0700

    hidden files

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """prints all names in hidden_4 that do not begin with __"""
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    for name in names:
+        if name[:2] != "__":
+            print(name)
