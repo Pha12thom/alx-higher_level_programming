38bc1585819c43d46528102b77c74b3d4c508b90
pha12thom <geofreymilugo@gmail.com>
2023-08-11 04:08:16 -0700

    Alternate prin ting

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
+for c in range(ord('a'), ord('z') - 1, -1):
     print("{:c}".format(c - 32) if c % 2 == 0 else "{:c}".format(c), end='')
 
