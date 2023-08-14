4095d5e2d5d7abd02f5258e65dad1b6665bea5df
pha12thom <geofreymilugo@gmail.com>
2023-08-14 11:49:21 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for num in my_list:
+        if idx < 0:
+            return None
+        if idx > num:
+            return None
+        else:
+            return num[idx]
