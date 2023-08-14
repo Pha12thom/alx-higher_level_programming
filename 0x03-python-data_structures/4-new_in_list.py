3cf21452f869a1721842306f3ceacae240e15836
pha12thom <geofreymilugo@gmail.com>
2023-08-14 13:50:32 -0700

    replacing order

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def new_in_list(my_list, idx, element):
-        if idx < 0 or num > len(my_list):
-            return my_list
-        my_list[idx] = element
+def new_in_list(my_list, idx, element):
+    if idx < 0 or num > len(my_list):
         return my_list
+    my_list[idx] = element
+    return my_list
