47cd03ce52ede394e83525e7b4cfd40b4cd1535a
pha12thom <geofreymilugo@gmail.com>
2023-08-14 13:41:42 -0700

    replacing values

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print("{:d}".format(num))
+if __name__ == "__main__":
+    def print_reversed_list_integer(my_list=[]):
+        for num in reversed(my_list):
+            print("{:d}".format(num))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or num > len(my_list):
+if __name__ == "__main__":
+    def new_in_list(my_list, idx, element):
+        if idx < 0 or num > len(my_list):
+            return my_list
+        my_list[idx] = element
         return my_list
-    my_list = my_list[:]
-    my_list[idx] = element
-    return my_list
