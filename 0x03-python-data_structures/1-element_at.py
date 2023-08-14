092305115352c2f1abc89f7fd8c85bb7d48dfeda
pha12thom <geofreymilugo@gmail.com>
2023-08-14 12:04:39 -0700

    fining the element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     for num in my_list:
         if idx < 0:
             return None
-        if idx > num:
+        if idx > len(my_list):
             return None
     return my_list[idx]
