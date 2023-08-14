b2f1a666a32854a6a00ed990101d798dcba70f41
pha12thom <geofreymilugo@gmail.com>
2023-08-14 13:31:29 -0700

    replacing value

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx<0 or num> len(my_list):
+    if idx < 0 or num > len(my_list):
         return my_list
     my_list = my_list[:]
     my_list[idx] = element
