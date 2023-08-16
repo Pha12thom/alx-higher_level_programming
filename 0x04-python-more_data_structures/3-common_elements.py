6c936c7b1e83b678aa1b82f91617f7ad4467001c
pha12thom <geofreymilugo@gmail.com>
2023-08-16 15:37:32 -0700

    common

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    if i in set_1 and i in set_2:
-        return i
+def only_diff_elements(set_1, set_2):
+    diff_elements = set()
+
+    for i in set_1:
+        if i not in set_2:
+            diff_elements.add(i)
+
+    for i in set_2:
+        if i not in set_1:
+            diff_elements.add(i)
+
+    return diff_elements
