889d5bc5b91764c6b1a41b4099dcb1b3c32a40e9
pha12thom <geofreymilugo@gmail.com>
2023-08-16 12:03:33 -0700

    squares of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[]]
-    for num in matrix:
-        num = num**2
-    new_matrix += num
-    return new_matrix
+    matrix = [[a, b, c]for i in range(a)for j in range(b)for k in range(c)]
+    return matrix**2
