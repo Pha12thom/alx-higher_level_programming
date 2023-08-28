096490cbff0a3ee7729ef14a9b0c2daa9b428ef1
pha12thom <geofreymilugo@gmail.com>
2023-08-28 13:28:09 -0700

    stdert

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,8 +9,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except Exception as e:
-            print(f"{e}")
+        except:
             result = b + a
             break
     return (result)
