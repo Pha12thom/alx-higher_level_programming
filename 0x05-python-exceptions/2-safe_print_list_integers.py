19755d721d4e61421f3070830fc3b3527d1a7b76
pha12thom <geofreymilugo@gmail.com>
2023-08-28 13:05:56 -0700

    new

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,6 @@ def safe_print_integer(value):
             print("{:d}".format(value))
         else:
             return false
-    except:
+    except Exception as e:
+        print(f"{e}")
         exit()
-  
-
-
