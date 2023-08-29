878deed6fcc9928d4e4d47440afa9e0b993441f0
pha12thom <geofreymilugo@gmail.com>
2023-08-29 13:19:56 -0700

    square values

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 class Square():
-    def __init__(self, length):
-        self.length = length
-
-    def perimeter(self):
-        return self.length*4
-
-    def Area(self):
-        return self.length**2
+    pass
