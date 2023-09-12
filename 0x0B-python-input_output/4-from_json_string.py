0da968d3cff9d7bd688af32a8d9cf97cb048c6e9
pha12thom <geofreymilugo@gmail.com>
2023-09-12 05:44:08 -0700

    json data structure

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""import json module"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    return data type for the python function
+    """
+    return json.loads(my_str)
