e22ab2c44c836bc364f52d4a8a5e4538137cdf4b
pha12thom <geofreymilugo@gmail.com>
2023-09-12 04:46:00 -0700

    file reader

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """File reader"""
 def read_file(filename=""):
+    """
+    pening the filebusing with finction
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         print(f, end="")
