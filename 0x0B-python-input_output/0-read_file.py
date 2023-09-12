7996ea235c430319d0d86745b01d73662d33618c
pha12thom <geofreymilugo@gmail.com>
2023-09-12 05:25:11 -0700

    read and write and json

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """File reader"""
+
+
 def read_file(filename=""):
-    """
-    pening the filebusing with finction
+    """pening the filebusing with finction
     """
     with open(filename, "r", encoding="UTF-8") as f:
         return f
---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""changing texts to string format"""
+
+
+def to_json_string(my_obj):
+    """
+    args(my_obj)
+    presenting string im json format
+    """
+    return json.dumps(my_obj)
