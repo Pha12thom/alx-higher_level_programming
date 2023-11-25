71aee63d3fa4724fc32ba422eac4f41f703e47ad
pha12thom <geofreymilugo@gmail.com>
2023-11-25 13:47:30 -0800

    arrays

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const x = 'C is fun';
+const y = 'Python is cool';
+const z = 'JavaScript is amazing';
+const myArray = [x, y, z];
+
+for (let i = 0; i < 3; i++) {
+  console.log(myArray[i]);
+}
